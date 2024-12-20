2a8656696e3fff60047c18b5303faba160b90777
Koga Miharu <j3328snm@sit.ac.jp>
2024-12-20 14:12:47 +0900

    桁数を調整

--- train2.py
+++ train2.py
@@ -79,7 +79,7 @@ for k in range(n_epoch):
     loss_train=models.train(model,dataloader_train,loss_fn,optimizer)
     time_end=time.time()
     loss_train_history.append(loss_train)
-    print(f'train loss:{loss_train:.3f}・({time_end-time_start}s)')
+    print(f'train loss:{loss_train:.3f}・({time_end-time_start:.1f}s)')
     loss_test=models.test(model,dataloader_test,loss_fn)    
     loss_test_history.append(loss_test)
     print(f'test loss:{loss_test:.3f}%')
